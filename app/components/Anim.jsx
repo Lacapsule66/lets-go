0711575914952a565aa40569d18bbd3c4b267b53
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2023-08-01 14:45:31 +0200

    Add files via upload

--- app/components/Anim.jsx
+++ app/components/Anim.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useEffect } from "react";
 import gsap from "gsap";
 
 
 export default function Anim() {
 
-
+const videoRef = useRef(null);
 
     useEffect(() => {
+     // if video is full loaded, play it
+      if (videoRef.current.readyState === 4) {
+        videoRef.current.play();
+      
+
      const tl = gsap.timeline();
         tl.to("#ok1", {
           delay: 1,
@@ -18,6 +23,7 @@ export default function Anim() {
             duration: .7,
             y: "-100%"
         }, 1.3)
+      }
     }, []);
 
     
@@ -26,7 +32,7 @@ export default function Anim() {
   return (
     <>
       <div id="ok1" className="bg-[#ffe95c] h-screen w-screen absolute top-0 z-50" >
-        <video className="h-full w-full object-cover" autoPlay muted loop src="https://res.cloudinary.com/dcl00yipc/video/upload/v1690840597/burgervideo_hhbyg3.mp4"></video>
+        <video ref={videoRef} className="h-full w-full object-cover" canplaythrough  muted loop src="https://res.cloudinary.com/dcl00yipc/video/upload/v1690840597/burgervideo_hhbyg3.mp4"></video>
       </div>
       <div id="ok" className="bg-pink-500 absolute w-screen h-screen z-40"></div>
     </>
